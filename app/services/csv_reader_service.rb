8f15b09891fdbae1a2de5f09dbf12fdb9b230eed
Italo Menna Silveira <italo.silveira@friday.ai>
2025-06-01 00:19:08 -0300

    feat: create csv reader

---
+++ app/services/csv_reader_service.rb
@@ -0,0 +1,16 @@
+require 'csv'
+
+class CsvReaderService
+  def initialize(filepath)
+    @filepath = filepath
+  end
+
+  def call
+    CSV.read(
+      @filepath,
+      col_sep: ';',
+      headers: true,
+      encoding: 'UTF-8'
+    ).map(&:to_h)
+  end
+end
